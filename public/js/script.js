313cba999bfe732f58ba427a12c863d712b01c06
jsostmann <jsostmann@aggies.ncat.edu>
2020-08-17 15:05:03 -0400

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    /*
+    let orgEven = "";
+    let orgOdd = "";
+
+    $("[class^=devicon-]:odd").on("mouseenter",function() {
+        orgOdd = $(this).css("color");
+        $(this).css("color","#FDB827");
+    });
+
+    $("[class^=devicon-]:odd").on("mouseleave",function() {
+        $(this).css("color",orgOdd);
+    });
+
+
+
+    $("[class^=devicon-]:even").on("mouseenter",function() {
+        orgEven = $(this).css("color");
+        $(this).css("color","#0d3068");
+    });
+
+    $("[class^=devicon-]:even").on("mouseleave",function() {
+        $(this).css("color",orgEven);
+    });
+    */
+
+
+});
